6a037248add98eb5793c4d03be923b633e05f391
heitor-hsantos <heitor.santos118@gmail.com>
2026-05-27 11:40:35 -0300

    Initial commit

---
+++ src/main/java/com/core/velocrm/VelocrmApplication.java
@@ -0,0 +1,13 @@
+package com.core.velocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelocrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/core/velocrm/VelocrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.velocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelocrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
