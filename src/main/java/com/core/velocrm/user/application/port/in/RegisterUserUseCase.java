a0e6f626e18adb4a4cf96c0b81ec2af3d3077076
heitor-hsantos <heitor.santos118@gmail.com>
2026-08-14 15:36:02 -0300

    refactor: remove unused configuration and security classes

--- src/main/java/com/core/velocrm/user/application/port/in/RegisterUserUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.core.velocrm.user.application.port.in;
-
-import com.core.velocrm.user.domain.model.User;
-import java.util.Set;
-
-public interface RegisterUserUseCase {
-    User registerUser(String email, String rawPassword, Set<String> roles);
-}
